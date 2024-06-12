9a09acbdf9f22d567c206b563109632b0974a350
SzymonDanza <szymonzlocki2001@gmail.com>
2024-06-12 19:23:17 +0200

    okno gry

--- main_1.py
+++ main_1.py
@@ -1,3 +1,27 @@
-szymon
-shdhash
-szymon2
+import  pygame,os,random
+
+pygame.init()
+
+SIZESCREEN = WIDTH, HEIGHT = 1366, 740
+screen = pygame.display.set_mode(SIZESCREEN)
+clock = pygame.time.Clock()
+
+path = os.path.join(os.getcwd(), 'img')
+file_names = os.listdir(path)
+LIGHTGREEN = pygame.color.THECOLORS['lightgreen']
+
+
+
+
+#pętla gry
+
+window_open = True
+while window_open:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                window_open = False
+        if event.type == pygame.QUIT:
+
+            window_open = False
+
